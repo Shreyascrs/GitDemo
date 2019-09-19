98536bd17b93a2554079a54400ccf50d85e2af0b
Shreyascrs <43531986+Shreyascrs@users.noreply.github.com>
2019-09-19 11:23:33 +0530

    changing in merge

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,5 +1,5 @@
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("welcome to today  and tomorrow and  session");
+        System.out.println("welcome to today  and tomorrow session");
     }
 }
